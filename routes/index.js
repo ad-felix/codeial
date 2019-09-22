0696e36097e1b87726d83c784733a0b874c4077c
Akash Das <41540459+ad-felix@users.noreply.github.com>
2019-09-22 07:45:32 +0530

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router =  express.Router();
 const homeController = require('../controllers/home_controller');
 
-console.log('router loaded');
+console.log('Router Loaded');
 
 router.get('/',homeController.home);
+router.use('/users', require('./users')); //Linking the root controller to the sub-controllers
+router.use('/users', require('./posts'));
+
+//for any further routes, access from here
+// reouter.use('/routerName', require('./routerFile'));
 
 module.exports = router;
